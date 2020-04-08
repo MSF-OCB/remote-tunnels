c4880d4ccaa43d513f76c9d8f2b9e6c5eb4a8284
RdN-MSF <ramses.de.norre@brussels.msf.org>
2020-04-08 02:32:03 +0200

    Automatically modify the nixos config files, make a commit, and push to github.

--- keygen.py
+++ keygen.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+import json
 import os
 import secrets
 import string
@@ -15,9 +16,10 @@ from itertools   import repeat, chain
 @dataclass(frozen=True)
 class KeyData:
   msf_location: str
-  port:         int
+  host:         str
   amount:       int
   user:         str
+  dry_run:      bool
   epoch_time:   int = int(time.time())
 
   def batch_name(self):
@@ -29,17 +31,28 @@ class KeyData:
   def key_file_name(self, key_id):
     return f"relay_{self.msf_location}_{key_id}"
 
+  def repo_path(self):
+    return os.path.join(self.batch_name(), "nixos")
+
+  def port(self):
+    if not os.path.isdir(self.repo_path()):
+      raise FileNotFoundError("NixOS repo not cloned!")
+    with open(os.path.join(self.repo_path(), "org-spec", "json", "tunnels.json"), 'r') as f:
+      tunnels = json.load(f)
+    return tunnels["tunnels"]["per-host"][self.host]["remote_forward_port"]
+
 def args_parser():
   def_key_amount=5
   parser = argparse.ArgumentParser(description='Generate keys and launch script for SSH tunnels.')
-  parser.add_argument('-l', '--location', type=str, required=True,  dest='msf_location',
+  parser.add_argument('-l', '--location', type=str,  required=True,  dest='msf_location',
                       help="The location of the MSF project, e.g. be_bruxelles")
-  parser.add_argument('-p', '--port',     type=int, required=True,  dest='port',
-                      help="The tunnel port of the remote server to which this key gives access, e.g. 6002")
-  parser.add_argument('-n', '--num',      type=int, required=False, dest='amount', default=def_key_amount, 
+  parser.add_argument('-s', '--server',   type=str,  required=True,  dest='host',
+                      help="The remote server to which this key will give access, e.g. benuc002")
+  parser.add_argument('-n', '--num',      type=int,  required=False, dest='amount', default=def_key_amount, 
                       help=f"The amount of keys to generate, defaults to {def_key_amount}")
-  parser.add_argument('-u', '--user',     type=str, required=False, dest='user',
+  parser.add_argument('-u', '--user',     type=str,  required=False, dest='user',
                       help="The user that will be used to connect with the generated keys, defaults to \"uf_<location>\"")
+  parser.add_argument('--dry-run',        type=bool, required=False, dest='dry_run', default=True)
   return parser
 
 def generate_passwd():
@@ -105,7 +118,7 @@ curl --connect-timeout 90 \\
      --retry 5 \\
      --location \\
      https://github.com/msf-ocb/remote-tunnels/raw/master/remote/create_tunnel.sh | \\
-  bash -s -- "{data.user}" "${{key_file}}" "{data.port}" "${{tmp_dir}}"
+  bash -s -- "{data.user}" "${{key_file}}" "{data.port()}" "${{tmp_dir}}"
 """
 
 def write_files(data, csvs, pub_keys, files):
@@ -125,22 +138,56 @@ def tar_files(tar_file_name, *files):
   with tarfile.open(tar_file_name, "w:gz") as tar:
     list(map(tar.add, files))
 
+def update_nixos_config(data, pub_keys):
+  rel_users_path = os.path.join("org-spec", "json", "users.json")
+  users_path = os.path.join(data.repo_path(), rel_users_path)
+  with open(users_path, 'r') as f:
+    users = json.load(f)
+  ensure_present(data.user, users["users"]["tunnel_only"]).sort()
+  ensure_present(data.user, users["users"]["per-host"][data.host]["enable"]).sort()
+  with open(users_path, 'w') as f:
+    json.dump(users, f, indent=2)
+  rel_key_path = os.path.join("org-spec", "keys", data.user)
+  key_path = os.path.join(data.repo_path(), rel_users_path)
+  with open(key_path, 'a+') as f:
+    f.write(*pub_keys)
+  commit_nixos_config(data, rel_users_path, rel_key_path)
+
+def commit_nixos_config(data, rel_users_path, rel_key_path):
+  subprocess.run(["git", "-C", data.repo_path(),
+                         "add", rel_users_path, rel_key_path])
+  subprocess.run(["git", "-C", data.repo_path(),
+                         "commit",
+                         "--author", "MSFOCB keygen script <msfocb_keygen@ocb.msf.org>",
+                         "--message", f"Commit keygen changes, batch id {data.batch_name()}"])
+  subprocess.run(["git", "-C", data.repo_path(), "pull", "--rebase"])
+  subprocess.run(["git", "-C", data.repo_path(), "push", "--dry-run" if data.dry_run else ""])
+
+def ensure_present(x, xs):
+  if x not in xs:
+    xs.append(x)
+  return xs
+
+def clone_nixos(data):
+  subprocess.run(["git", "clone", "git@github.com:MSF-OCB/NixOS.git", data.repo_path()])
+
 def print_info(data):
   print(f"\nCreated batch: {data.batch_name()}\n")
-  print( "@nixos repo, do not forget to:")
-  print(f"- Copy (or add the content of) {data.user} to org-spec/keys")
-  print(f"- Add {data.user}.enable = true; to the users.users object @org-spec/hosts/benucXXX.nix (port={data.port})")
-  print(f"- Add {data.user} = tunnelOnly; to org-spec/ocb_users.nix")
-  print(f"- Commit, push, pull and nixos-rebuild on the remote server {data.port}")
+  print( "Do not forget to:")
+  print(f"- Run nixos-rebuild on the remote server {data.host}")
   print( "- Add the keys to keeper")
 
 def go():
   args = args_parser().parse_args()
-  data = KeyData(args.msf_location, args.port, args.amount, args.user or "uf_" + args.msf_location)
+  data = KeyData(args.msf_location, args.host, args.amount, args.user or "uf_" + args.msf_location, args.dry_run)
 
   os.mkdir(data.batch_name())
+  clone_nixos(data)
+
   (csvs, pub_keys, key_files) = generate_keys(data)
   write_files(data, csvs, pub_keys, key_files)
+  update_nixos_config(data, pub_keys)
+
   list(map(os.remove, key_files))
   print_info(data)
 
