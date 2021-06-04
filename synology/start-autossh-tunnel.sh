a26bfc62a1d78094f93e44acf21a8cb4c30ea5bc
Ramses <141248+R-VdP@users.noreply.github.com>
2021-06-04 15:14:05 +0200

    Adding a script to start tunnels on synology. (#6)

---
+++ synology/start-autossh-tunnel.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+relay_port="${1}"
+relay="${2}"
+priv_key="${3}"
+
+if [ -z "${relay_port}" ] || [ -z "${relay}" ] || [ -z "${priv_key}" ]; then
+  echo "Usage:"
+  echo "  ${0} <relay_port> <relay_host> <priv_key>"
+  exit 1
+fi
+
+echo "Starting tunnel..."
+
+autossh \
+  -M 0 \
+  -T -N \
+  -o "ExitOnForwardFailure=yes" \
+  -o "ServerAliveInterval=10" \
+  -o "ServerAliveCountMax=5" \
+  -o "ConnectTimeout=360" \
+  -o "UpdateHostKeys=no" \
+  -o "StrictHostKeyChecking=yes" \
+  -o "UserKnownHostsFile=/root/.ssh/known_hosts" \
+  -o "IdentitiesOnly=yes" \
+  -o "Compression=yes" \
+  -o "ControlMaster=no" \
+  -R "${relay_port}":localhost:22 \
+  -i "${priv_key}" \
+  -p 443 \
+  -l tunnel \
+  "${relay}"
+
+echo "Tunnel stopped..."
+
