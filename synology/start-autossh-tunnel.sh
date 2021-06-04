548f9f41b2b290c54d397cdef68495af5a52a1c9
R-VdP <141248+R-VdP@users.noreply.github.com>
2021-06-04 17:13:02 +0200

    Include the relay keys.

--- synology/start-autossh-tunnel.sh
+++ synology/start-autossh-tunnel.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
 
+sshrelay_key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDsn2Dvtzm6jJyL9SJY6D1/lRwhFeWR5bQtSSQv6bZYf"
+
 relay_port="${1}"
 relay="${2}"
 priv_key="${3}"
+
 
 if [ -z "${relay_port}" ] || [ -z "${relay}" ] || [ -z "${priv_key}" ]; then
   echo "Usage:"
@@ -10,11 +13,26 @@ if [ -z "${relay_port}" ] || [ -z "${relay}" ] || [ -z "${priv_key}" ]; then
   exit 1
 fi
 
+
+if [ -z "${tmp_dir}" ]; then
+  tmp_dir=$(mktemp -d)
+fi
+
+known_hosts_file="${tmp_dir}/known_hosts"
+
+cat <<EOF > "${known_hosts_file}"
+${relay}       ${sshrelay_key}
+[${relay}]:80  ${sshrelay_key}
+[${relay}]:443 ${sshrelay_key}
+EOF
+
+
 echo "Waiting for autossh to become available..."
 
 while [ ! -x /usr/local/bin/autossh ]; do
   sleep 10
 done
+
 
 echo "Starting tunnel..."
 
@@ -27,7 +45,7 @@ echo "Starting tunnel..."
   -o "ConnectTimeout=360" \
   -o "UpdateHostKeys=yes" \
   -o "StrictHostKeyChecking=yes" \
-  -o "UserKnownHostsFile=/root/.ssh/known_hosts" \
+  -o "UserKnownHostsFile=${known_hosts_file}" \
   -o "IdentitiesOnly=yes" \
   -o "Compression=yes" \
   -o "ControlMaster=no" \
