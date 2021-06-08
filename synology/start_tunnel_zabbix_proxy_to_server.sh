fe05283d57129d31bfa576d02bf968730194c61f
Nour Al Konsol <73284505+nourkonsol@users.noreply.github.com>
2021-06-08 16:59:15 +0200

    working on proxy command

--- synology/start_tunnel_zabbix_proxy_to_server.sh
+++ synology/start_tunnel_zabbix_proxy_to_server.sh
@@ -1,8 +1,14 @@
 #! /usr/bin/env bash
 server_key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAQzSeGYj0r2P80lGa6qgRGi1OBgJ4q0qvXy+PlZ3BuP"
-#ED25519 key fingerprint is SHA256:6Dqg2zfVE35WSP49rzCgzk7y1/A0ZE+hxBBhLtwkqwE 
+sshrelay="sshrelay.ocb.msf.org"
+sshrelay_ip="15.188.17.148,185.199.180.11,2a05:d012:209:9a00:8e2a:9f6c:53be:df41"
+sshrelay_key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDsn2Dvtzm6jJyL9SJY6D1/lRwhFeWR5bQtSSQv6bZYf"
+
+declare -a relay_ports=("443" "22" "80")
+
 zabbix_server_port="${1}"
 priv_key="${2}"
+
 
 if [ -z "${zabbix_server_port}" ]  || [ -z "${priv_key}" ]; then
   echo "Usage:"
@@ -14,11 +20,12 @@ if [ -z "${tmp_dir}" ]; then
   tmp_dir=$(mktemp -d)
 fi
 
-known_hosts_file_server="${tmp_dir}/known_hosts"
+known_hosts_file="${tmp_dir}/known_hosts"
 
-cat <<EOF > "${known_hosts_file_server}"
+cat <<EOF > "${known_hosts_file}"
 ${localhost}       ${server_key}
 [${localhost}]:7105  ${server_key}
+${sshrelay},${sshrelay_ip} ${sshrelay_key}
 EOF
 
 echo "Waiting for autossh to become available..."
@@ -29,25 +36,43 @@ done
 
 echo "Starting tunnel to Server..."
 
+ssh_common_options="-F none \
+                    -o ServerAliveInterval=10 \
+                    -o ServerAliveCountMax=5 \
+                    -o ConnectTimeout=360 \
+                    -o LogLevel=ERROR \
+                    -o AddKeysToAgent=yes"
+                    
+for repeat in $(seq 1 20); do
+  for relay_port in "${relay_ports[@]}"; do
+  
 /usr/local/bin/autossh \
   -M 0 \
   -N -T \
   -L 10052:localhost:10051 \
+  ${ssh_common_options} \
   -i "${priv_key}" \
-  -o ProxyCommand="ssh -W %h:%p tunneller@sshrelay.ocb.msf.org" \
-                             -o "ExitOnForwardFailure=yes" \
-                             -o "ServerAliveInterval=10" \
-                             -o "ServerAliveCountMax=5" \
-                             -o "ConnectTimeout=360" \
-                             -o "UpdateHostKeys=yes" \
-                             -o "StrictHostKeyChecking=yes" \
-                             -o "IdentitiesOnly=yes" \
-                             -o "Compression=yes" \
-                             -o "ControlMaster=no" \
-                             -o "UserKnownHostsFile=${known_hosts_file_server}" \
-                             -o "LogLevel=ERROR" \
-                             -o "AddKeysToAgent=yes" \
-  zbx_tnl@localhost \
+  -o "ExitOnForwardFailure=no" \
+  -o ProxyCommand="ssh -W %h:%p \
+                                  -i "${priv_key}" \
+                                  ${ssh_common_options} \
+                                  -o StrictHostKeyChecking=yes \
+                                  -o UserKnownHostsFile=${known_hosts_file} \
+                                  -p ${relay_port} \
+                                  -l tunneller \
+                                  ${sshrelay}" \
+    -o "ExitOnForwardFailure=yes" \
+#     -o "ServerAliveInterval=10" \
+#     -o "ServerAliveCountMax=5" \
+#     -o "ConnectTimeout=360" \
+#     -o "UpdateHostKeys=yes" \
+#     -o "StrictHostKeyChecking=yes" \
+     -o "IdentitiesOnly=yes" \
+     -o "Compression=yes" \
+     -o "ControlMaster=no" \
+     -o "UserKnownHostsFile=/dev/null" \
+     -l zbx_tnl \
+     localhost \
   -i "${priv_key}" \
   -p "${zabbix_server_port}"
 
