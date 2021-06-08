d3164bb9a2c84ae98189dc34d22e74518ce02ced
Nour Al Konsol <73284505+nourkonsol@users.noreply.github.com>
2021-06-08 12:33:38 +0200

    changed the ssh options
    I didnot mange the public key
    Waiting for autossh to become available...
    Starting tunnel to Server...
    No ED25519 host key is known for [localhost]:7105 and you have requested strict checking.
    Host key verification failed.
    Killed by signal 1.
    Stopping the tunnel to Server..
    
    when I run the script on StrictHostKeyChecking=ask
    root@Nestor:/usr/local/bin# start_tunnel_to_server_2.sh 7105 /root/id_tunnel
    Waiting for autossh to become available...
    Starting tunnel to Server...
    The authenticity of host '[localhost]:7105 (<no hostip for proxy command>)' can't be established.
    ED25519 key fingerprint is SHA256:6Dqg2zfVE35WSP49rzCgzk7y1/A0ZE+hxBBhLtwkqwE.
    Are you sure you want to continue connecting (yes/no/[fingerprint])?
    then it work . can you please check the script ?

--- synology/start_tunnel_zabbix_proxy_to_server.sh
+++ synology/start_tunnel_zabbix_proxy_to_server.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
-
+server_key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAQzSeGYj0r2P80lGa6qgRGi1OBgJ4q0qvXy+PlZ3BuP"
+#ED25519 key fingerprint is SHA256:6Dqg2zfVE35WSP49rzCgzk7y1/A0ZE+hxBBhLtwkqwE 
 zabbix_server_port="${1}"
 priv_key="${2}"
 
@@ -12,6 +13,13 @@ fi
 if [ -z "${tmp_dir}" ]; then
   tmp_dir=$(mktemp -d)
 fi
+
+known_hosts_file_server="${tmp_dir}/known_hosts"
+
+cat <<EOF > "${known_hosts_file_server}"
+${localhost}       ${server_key}
+[${localhost}]:7105  ${server_key}
+EOF
 
 echo "Waiting for autossh to become available..."
 
@@ -27,17 +35,20 @@ echo "Starting tunnel to Server..."
   -L 10052:localhost:10051 \
   -i "${priv_key}" \
   -o ProxyCommand="ssh -W %h:%p tunneller@sshrelay.ocb.msf.org" \
-  -o "ExitOnForwardFailure=yes" \
-  -o "ServerAliveInterval=10" \
-  -o "ServerAliveCountMax=5" \
-  -o "ConnectTimeout=360" \
-  -o "UpdateHostKeys=yes" \
-  -o "StrictHostKeyChecking=yes" \
-  -o "IdentitiesOnly=yes" \
-  -o "Compression=yes" \
-  -o "ControlMaster=no" \
+                             -o "ExitOnForwardFailure=yes" \
+                             -o "ServerAliveInterval=10" \
+                             -o "ServerAliveCountMax=5" \
+                             -o "ConnectTimeout=360" \
+                             -o "UpdateHostKeys=yes" \
+                             -o "StrictHostKeyChecking=yes" \
+                             -o "IdentitiesOnly=yes" \
+                             -o "Compression=yes" \
+                             -o "ControlMaster=no" \
+                             -o "UserKnownHostsFile=${known_hosts_file_server}" \
+                             -o "LogLevel=ERROR" \
+                             -o "AddKeysToAgent=yes" \
   zbx_tnl@localhost \
   -i "${priv_key}" \
   -p "${zabbix_server_port}"
 
-echo "Stopping tunnel to Server..."
+echo "Stopping the tunnel to Server..."
